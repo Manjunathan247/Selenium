4d745822b263d4ec08c36c9fb820ed7a023a10ad
Manjunathan247 <42131602+Manjunathan247@users.noreply.github.com>
2020-04-27 19:44:52 +0530

    27-04-2020
    Commit

--- src/com/practice/DragAndDrop.java
+++ src/com/practice/DragAndDrop.java
@@ -14,7 +14,7 @@ public class DragAndDrop {
 		System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://www.dhtmlgoodies.com/submitted-scripts/i-google-like-drag-drop/index.html");
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		WebElement src = driver.findElement(By.xpath("//h1[text()='Block 1']"));
 		WebElement trg = driver.findElement(By.xpath("//div[@id='column-3']"));
 		Actions actions=new Actions(driver);
